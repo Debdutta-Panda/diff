aeaf945f0dd79085575d0cdd4be31a4554f23c43
Debdutta Panda <92369023+Debdutta-Panda@users.noreply.github.com>
2022-10-23 17:42:42 +0530

    Update test.kt

--- test.kt
+++ test.kt
@@ -3,21 +3,30 @@ Column(
     horizontalAlignment = Alignment.CenterHorizontally
 ) {
     TextField(
-        value = "",
-        onValueChange = {},
+        value = input1.value,
+        onValueChange = {
+            input1.value = it
+        },
         placeholder = {
             Text("Input1")
         }
     )
     TextField(
-        value = "",
-        onValueChange = {},
+        value = input2.value,
+        onValueChange = {
+            input2.value = it
+        },
         placeholder = {
             Text("Input2")
         }
     )
-    Button(onClick = { /*TODO*/ }) {
+    Button(onClick = {
+        val input1Number = input1.value.toInt()
+        val input2Number = input2.value.toInt()
+        val resultNumber = input1Number + input2Number
+        result.value = resultNumber.toString()
+    }) {
         Text("Click Me")
     }
-    Text("Result:")
+    Text("Result:${result.value}")
 }
